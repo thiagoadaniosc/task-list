d90ada4fb562a4afa8560889006c0d1870f2253a
Thiago Scheidt <thiagoadaniosc@gmail.com>
2019-01-11 13:09:04 -0200

    Primeiro Commit

---
+++ app/Http/Controllers/TaskListController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskListController extends Controller
+{
+    public function index(){
+
+    }
+
+    public function show($id){
+        
+    }
+}
